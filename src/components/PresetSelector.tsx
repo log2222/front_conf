3860415239a8923945604eb0a684a704dec6c309
log2222 <82288389+log2222@users.noreply.github.com>
2025-06-27 20:10:47 +0500

    Update PresetSelector.tsx

--- src/components/PresetSelector.tsx
+++ src/components/PresetSelector.tsx
@@ -15,7 +15,7 @@ const PresetSelector: React.FC<Props> = ({ onSelect }) => {
   const [presets, setPresets] = useState<Preset[]>([]);
 
   useEffect(() => {
-    axios.get('http://localhost:8000/presets')
+    axios.get('https://bconf.onrender.com/presets')
       .then(res => setPresets(res.data));
   }, []);
 
